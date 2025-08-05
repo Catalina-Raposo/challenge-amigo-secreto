358cec8b68c759dc003b8292863786351836b74b
Catalina-Raposo <catalina.raposo@usach.cl>
2025-08-04 22:27:06 -0400

    Funcion de agregar nuevo nombre de amigo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # challenge-amigo-secreto
+Crear un array para almacenar los nombres ej: let amigos = []
+Implementar una función para agregar amigos:
+    -Capturar el valor del campo de entrada
+    -Validar la entrada
+    -Actualizar el array de amigos
+    -Limpiar el campo de entrada
+
--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+console.log(amigos)
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo")
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+
+
+    if(nombre === ""){
+        alert("Se debe agregar un nombre");
+        return;
+    }
+    
+    if(amigos.includes(nombreAmigo)){ 
+        alert(`El nombre ${nombreAmigo} ya ha sido ingresado, ingrese uno nuevo`);
+        return;
+    }
+     
+    amigos.push(nombreAmigo);
+
+    inputAmigo.ariaValueMax ="";
+
+    actualizarLista()
+
+    
+}
